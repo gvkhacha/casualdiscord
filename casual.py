15b761666ac9d765c70aa12787c36b56fb041838
Garik Khachatryan <gvkhacha@uci.edu>
2019-09-20 11:17:34 -0700

    basic config for bot

--- casual.py
+++ casual.py
@@ -1,23 +1,65 @@
-import discord
+import discord, re
+from datetime import datetime
 import prompts
-# For now just using api token in text file, later config file
 
+validRegex = re.compile('(\d{1,2})([smhd])')
+
+# For now just using api token in text file, later config file
 with open("token.txt", "r") as f:
     TOKEN = f.read().strip()
 
-print(TOKEN)
+CONVERT = {'s': 1, 'm': 60, 'h': 3600}
 
-class MyClient(discord.Client):
+def stringToTime(match: 're.match') -> int:
+    # returns time in ms from stringed match in SECONDS
+    try:
+        time = int(match.group(1))
+        assert time != 0
+
+        return time * CONVERT[match.group(2)]
+    except (ValueError, AssertionError):
+        return 0
+
+class CasualDiscord(discord.Client):
+
     async def on_ready(self):
+        self.config = dict()
         print('Logged on as', self.user)
 
     async def on_message(self, message):
-        # don't respond to ourselves
         if message.author == self.user:
             return
 
-        if message.content == '$casual':
-            await message.channel.send('PM this bot to enable casual discord notifications')
+        if type(message.channel) == discord.channel.DMChannel:
+            await self.handle_pm(message)
 
-client = MyClient()
+        if message.content == '$casual':
+            await message.channel.send(prompts.INIT)
+        elif message.author.id in self.config.keys():
+            self.config[message.author.id]['channels'][message.channel.id] = datetime.now().timestamp()
+        else:
+            await self.handle_notif(message)
+
+    async def handle_notif(self, message):
+        for userId, config in self.config.items():
+            if message.channel.id in config['channels']:
+                start = datetime.fromtimestamp(config['channels'][message.channel.id])
+                print(start)
+                print(message.created_at)
+
+    async def handle_pm(self, message):
+        result = re.match(validRegex, message.content.lower())
+
+        if(result):
+            time = stringToTime(result)
+
+            if(time == 0):
+                await message.channel.send(prompts.INVALID_TIME)
+            else:
+                self.config[message.author.id] = {'time': time, "channels": dict()}
+                await message.channel.send(prompts.CONFIRM.format(' '.join(result.groups())))
+        else:
+            await message.channel.send(prompts.INVALID_TIME)
+
+client = CasualDiscord()
 client.run(TOKEN)
---
+++ prompts.py
@@ -0,0 +1,3 @@
+INIT = "PM this bot with a timeframe (5m, 10m, 1h) to enable casual discord notifications"
+INVALID_TIME = "That is not a correct time frame. Please enter a number 1-99 and time unit (s, m, d)"
+CONFIRM = "Okay, I will forward messages to you for {} after you send a message on that channel"
